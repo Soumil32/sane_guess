a3cf327152858edc42ac9ae597cbc17ef71eb166
Soumil Gupta <soumil009@outlook.com>
2026-06-24 12:30:29 +0100

    stage 2

--- llm.py
+++ llm.py
@@ -1,8 +1,23 @@
-import openai
+import ollama
 
 class LLM:
 
-    def __init__(self, api_key, base_url=None):
+    def __init__(self, api_key, base_url, model, system_prompt):
         self.api_key = api_key
-        self.base_url = base_url or "https://api.openai.com/v1"
-        
+        self.base_url = base_url
+        self.model = model
+        self.system_prompt = system_prompt
+
+    def chat(self, message: str, response_format):
+        response: ollama.ChatResponse = ollama.chat(model=self.model, messages=[
+            {
+                'role': 'system',
+                'content': self.system_prompt,
+            },
+            {
+                'role': 'user',
+                'content': message,
+            },
+        ])
+
+        return response.message.content
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import wikipedia
+import llm
 
 def main():
-    query = input("Enter a search query: ")
-    
+    with open("prompt/keyword_extractor.txt", "r") as f:
+        extractor_prompt = f.read()
+    # query = input("Enter a search query: ")
+    query = "python is a popular beginner programming language"
+    keyword_extractor = llm.LLM(None, "http://localhost:11434/api", "hf.co/unsloth/Qwen3.5-4B-GGUF:Q4_K_S", extractor_prompt)
+    keywords = keyword_extractor.chat(query, None)
+    print("Extracted keywords:", keywords)
 
 if __name__ == "__main__":
     main()
